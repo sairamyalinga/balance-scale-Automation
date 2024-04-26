bfd6ccec5eabd4e7265ebcf10380b44e8b6edffd
Sai Ramya Linga <sairamyalinga@gmail.com>
2024-04-25 20:14:52 -0700

    Refactor

--- main.py
+++ main.py
@@ -1,127 +1,97 @@
+import time
+import ast
+import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-import ast
+
+def reset_button():
+    driver.find_element(By.XPATH, "//*[text()='Reset']").click()
+
+def get_equation(n):
+    eq = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, f"//ol//li[{n}]"))
+    )
+    return eq.text
+def add_element(id, key):
+    driver.find_element(By.ID, id).send_keys(key)
+
+def result(ans):
+    div_coins = driver.find_element(By.CLASS_NAME, "coins")
+    button = div_coins.find_element(
+                    By.XPATH, f".//button[text()='{ans}']")
+    button.click()
+
+
 
 try:
     driver = webdriver.Chrome(service=ChromeService(
         ChromeDriverManager().install()))
 
-    driver.get("http://sdetchallenge.fetch.com/")
-
-    
-    driver.find_element(By.ID, "left_0").send_keys(0)
-    driver.find_element(By.ID, "left_1").send_keys(1)
-    driver.find_element(By.ID, "left_2").send_keys(2)
-    driver.find_element(By.ID, "right_0").send_keys(3)
-    driver.find_element(By.ID, "right_1").send_keys(4)
-    driver.find_element(By.ID, "right_2").send_keys(5)
-    
-    
+    driver.get("http://sdetchallenge.fetch.com/")  
+    add_element("left_0", 0)
+    add_element("left_1", 1)
+    add_element("left_2", 2)
+    add_element("right_0", 3)
+    add_element("right_1", 4)
+    add_element("right_2", 5)
     weigh = driver.find_element(By.ID, "weigh")
     weigh.click()
-    eq = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//ol//li[1]"))
-    )
-    print(eq.text)
-    tt = eq.text
-
-    print(driver.find_element(By.ID, "reset"))
-    driver.find_element(By.ID, "reset").click()
-
+    tt = get_equation(1)
+    print(tt)
     if "=" in tt:
-        driver.find_element(By.XPATH, "//*[text()='Reset']").click()
-        driver.find_element(By.ID, "left_0").send_keys(6)
-        driver.find_element(By.ID, "right_0").send_keys(7)
+        reset_button()
+        add_element("left_0", 6)
+        add_element("right_0", 7)
         weigh.click()
-        neweq = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, "//ol//li[2]")))
-        if neweq:
-            ntt = neweq.text
-            if "=" in ntt:
-                div_coins = driver.find_element(By.CLASS_NAME, "coins")
-                button = div_coins.find_element(
-                        By.XPATH, ".//button[text()='8']")
-                button.click()
-            else:
-                if "<" in ntt:
-                    div_coins = driver.find_element(By.CLASS_NAME, "coins")
-                    button = div_coins.find_element(
-                        By.XPATH, ".//button[text()='6']")
-                    button.click()
-                if ">" in ntt:
-                    div_coins = driver.find_element(By.CLASS_NAME, "coins")
-                    button = div_coins.find_element(
-                        By.XPATH, ".//button[text()='7']")
-                    button.click()
-        else:
-            print("None found")
-            driver.quit()
+        ntt = get_equation(2)   
+        if "=" in ntt:
+            result(8)
+        if "<" in ntt:
+            result(6)
+        if ">" in ntt:
+            result(7)
+
     if "<" in tt:
         expressions = tt.split("<")
         lhs = expressions[0].strip()
         lhs = ast.literal_eval(lhs)
-        driver.find_element(By.XPATH, "//*[text()='Reset']").click()
-        driver.find_element(By.ID, "left_0").send_keys(lhs[0])
-        driver.find_element(By.ID, "right_0").send_keys(lhs[1])
+        reset_button()
+        add_element("left_0", lhs[0])
+        add_element("right_0", lhs[1])
         weigh.click()
-        neweq = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//ol//li[2]")))
-        ntt = neweq.text
+        ntt =get_equation(2)
         if "=" in ntt:
-            div_coins = driver.find_element(By.CLASS_NAME, "coins")
-            button = div_coins.find_element(
-                By.XPATH, f".//button[text()='{lhs[2]}']")
-            button.click()
+            result(lhs[2])
         if "<" in ntt:
-            div_coins = driver.find_element(By.CLASS_NAME, "coins")
-            button = div_coins.find_element(
-                By.XPATH, f".//button[text()='{lhs[0]}']")
-            button.click()
+            result(lhs[0])
         if ">" in ntt:
-            div_coins = driver.find_element(By.CLASS_NAME, "coins")
-            button = div_coins.find_element(
-                By.XPATH, f".//button[text()='{lhs[1]}']")
-            button.click()
+           result(lhs[1])
     if ">" in tt:
         expressions = tt.split(">")
         rhs = expressions[1].strip()
         print(rhs)
         rhs = ast.literal_eval(rhs)
-        driver.find_element(By.XPATH, "//*[text()='Reset']").click()
-        driver.find_element(By.ID, "left_0").send_keys(rhs[0])
-        driver.find_element(By.ID, "right_0").send_keys(rhs[1])
+        reset_button()
+        add_element("left_0", rhs[0])
+        add_element("right_0", rhs[1])
         weigh.click()
-        neweq = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//ol//li[2]")))
-        ntt = neweq.text
+        ntt = get_equation(2)
         if "=" in ntt:
-            div_coins = driver.find_element(By.CLASS_NAME, "coins")
-            button = div_coins.find_element(
-                By.XPATH, f".//button[text()='{rhs[2]}']")
-            button.click()
+            result(rhs[2])    
         if "<" in ntt:
-            div_coins = driver.find_element(By.CLASS_NAME, "coins")
-            button = div_coins.find_element(
-                By.XPATH, f".//button[text()='{rhs[0]}']")
-            button.click()
+            result(rhs[0])
         if ">" in ntt:
-            div_coins = driver.find_element(By.CLASS_NAME, "coins")
-            button = div_coins.find_element(
-                By.XPATH, f".//button[text()='{rhs[1]}']")
-            button.click()
+            result(rhs[1])
+            
 
 
 except Exception as e:
-    print(f"An error occurred: {str(e)}")
+    logging.error(f"An error occurred: {str(e)}")
 
 finally:
-    time.sleep(100)
-    print(driver.title)
-    print(driver.current_url)
+    time.sleep(5)
     driver.quit()
